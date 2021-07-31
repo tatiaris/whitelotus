87f420b399dc39d06b8360df81eb13c65b58cd37
tatiaris <tatiaris2001@gmail.com>
2021-07-31 11:15:59 -0500

    link fix

--- util/socket.js
+++ util/socket.js
@@ -1,7 +1,6 @@
 import io from 'socket.io-client';
 
-const socketUrl = "http://localhost:8888/";
-// const socketUrl = (process.env.ENVIRONMENT === "test") ? "http://localhost:8888/" : "https://rtbgs.herokuapp.com/"
+const socketUrl = "https://rtbgs.herokuapp.com/";
 
 export const socket = io(socketUrl, {
   withCredentials: true,
