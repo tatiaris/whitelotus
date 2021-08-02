f3af100541638b6fa909800c796edb2a881e8cf2
tatiaris <tatiaris2001@gmail.com>
2021-08-02 01:32:48 -0500

    typo fix

--- util/socket.js
+++ util/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socketUrl = 'http://localhost:8888';
+const socketUrl = 'https://rtbgs.herokuapp.com';
 
 export const socket = io(socketUrl, {
   withCredentials: true,
